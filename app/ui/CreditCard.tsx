8c4dec60c69d30641e779f577c942cf94381c66d
jaaniles <jaani.leskinen@fraktio.fi>
2024-07-11 14:26:23 +0300

    feat: inspect card feature

--- app/ui/CreditCard.tsx
+++ app/ui/CreditCard.tsx
@@ -1,5 +1,6 @@
 import stylex from "@stylexjs/stylex";
 import { motion } from "framer-motion";
+import { useState } from "react";
 import { User } from "~/auth/auth";
 import { colors, spacing } from "~/tokens.stylex";
 
@@ -11,24 +12,48 @@ type Props = {
 const CARD_HEIGHT = 190;
 
 export function CreditCard({ user, vertical }: Props) {
+  const [inspect, setInspect] = useState(false);
+
+  const handleInspect = () => {
+    if (vertical) {
+      setInspect(!inspect);
+    }
+  };
+
+  const animate = inspect ? "inspect" : vertical ? "vertical" : "horizontal";
+
   return (
     <motion.div
+      onClick={handleInspect}
       layoutId="credit-card"
       initial="horizontal"
-      animate={vertical ? "vertical" : "horizontal"}
+      whileHover={"hover"}
+      animate={animate}
       variants={{
         horizontal: {
+          scale: 1,
           x: 0,
           rotate: 0,
         },
         vertical: {
+          scale: 1,
           rotate: 90,
           x: -CARD_HEIGHT * 1.75,
+        },
+        inspect: {
+          scale: 1,
+          rotate: 7,
+          x: -CARD_HEIGHT / 2.25,
+        },
+        hover: {
+          y: -5,
+          scale: 1.01,
         },
       }}
       {...stylex.props(
         styles.root,
-        vertical ? styles.vertical : styles.horizontal
+        inspect && styles.inspect,
+        styles[vertical ? "vertical" : "horizontal"]
       )}
     >
       <div>
@@ -65,6 +90,7 @@ const styles = stylex.create({
     boxShadow: `0px 0px 18px 0px rgba(88,53,59,0.8)`,
   },
   horizontal: {
+    maxWidth: "95vw",
     width: 400,
     height: CARD_HEIGHT + 35,
   },
@@ -75,6 +101,9 @@ const styles = stylex.create({
   },
   alignRight: {
     textAlign: "right",
+  },
+  inspect: {
+    position: "absolute",
   },
   text: {
     fontWeight: 500,
